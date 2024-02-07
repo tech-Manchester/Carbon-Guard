658e053a70dfdfd153cfa493acd38c7a70da15d4
Akshat  Mahajan <140583575+tech-Manchester@users.noreply.github.com>
2024-02-07 10:56:07 +0530

    Update url.py

--- CG/CEC/url.py
+++ CG/CEC/url.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('',views.home, name="home"),
     path('login',views.login, name="login"),
-    
-
+    path('signup',views.signup, name="signup"),
+    path('postsign/',views.postsign,name ="postsign"),
+    path('presign/',views.presign,name ="presign"),
+    path('calculator',views.calculator, name="calculator"),
 ]
